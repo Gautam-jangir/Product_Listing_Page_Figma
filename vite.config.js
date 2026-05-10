b4a38b4c3384f66f6c02afd4a8348dcddde9b5f9
ashishjangir660 <ashishjangir660@gmail.com>
2026-05-10 22:28:22 +0530

    fix base url

--- vite.config.js
+++ vite.config.js
@@ -5,5 +5,5 @@ import tailwindcss from "@tailwindcss/vite";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: '/Product_Listing_Page_Figma/'
+  base: '/'
 });
